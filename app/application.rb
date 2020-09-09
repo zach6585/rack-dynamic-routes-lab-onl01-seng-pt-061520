fcb6ddfd406ef09f409082d237e6cac38d5e6cd6
zachary mines <zmines@wesleyan.edu>
2020-09-09 00:20:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     req = Rack::Request.new(env)
     if req.path.match(/items/)
       @@items.each do |it|
-        
+        if it.name.to_s = req.path.to_s[7...]
       else 
         resp.write "Item not found"
         resp.status = 400
