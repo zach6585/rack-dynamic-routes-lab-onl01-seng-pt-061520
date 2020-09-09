296d7bb5de68bff97a371193d393a68781c33317
zachary mines <zmines@wesleyan.edu>
2020-09-09 00:23:48 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,24 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    if req.path.match(/items/)
+      i = nil 
+      @@items.each do |it|
+        if it.name.to_s == req.path.to_s[7...]
+          i = it 
+        end 
+      end 
+      if i != nil 
+        resp.write "#{i.price}"
+      else 
+        resp.write "Item not found"
+        resp.status = 400
+      end 
+    else 
+      resp.write "Route not found"
+      resp.status = 404
+    end 
+    resp.finish
+  end 
+end 
