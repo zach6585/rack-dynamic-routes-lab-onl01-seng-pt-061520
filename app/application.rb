abdaad3c72ffc786975c9ecfb7f201b90e0fbfde
zachary mines <zmines@wesleyan.edu>
2020-09-09 00:19:46 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,23 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    if req.path.match(/items/)
+      allo = req.params["q"]
+      req.each do |par|
+        resp.write "#{par}"
+      end 
+      # resp.write "#{allo}"
+    #   if @@items.include?(allo)
+    #     resp.write "#{allo}"
+    #   else 
+    #     resp.write "Item not found"
+    #     resp.status = 400
+    #   end 
+    # else 
+    #   resp.write "Route not found"
+    #   resp.status = 404
+    end 
+    resp.finish
+  end 
+end 
